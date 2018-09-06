3b79004d5cdff6b9d7536ce2df92d3d3b934463c
jeromesnail <jeromesnail@hotmail.fr>
2018-09-06 18:05:02 +0200

    Added index.php (empty), .gitignore and model Manager.php

---
+++ models/Manager.php
@@ -0,0 +1,10 @@
+<?php
+
+require_once('../config.php');
+
+abstract class Manager {
+
+  protected function dbConnect() {
+    return new PDO('mysql:host=' . DB_HOST . ';dbname=' . DB_NAME . ';charset=' . DB_CHARSET, DB_USER, DB_PASSWORD);
+  }
+}
