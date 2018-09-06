20a218ed81aa4d526332ddd470936e121359d10b
jeromesnail <jeromesnail@hotmail.fr>
2018-09-06 19:28:46 +0200

    Added description to the dbConnect method (Manager class)

--- models/Manager.php
+++ models/Manager.php
@@ -2,8 +2,13 @@
 
 require_once('../config.php');
 
+
 abstract class Manager {
 
+  /** Connection to database.
+   * 
+   * @return PDO
+  */
   protected function dbConnect() {
     return new PDO('mysql:host=' . DB_HOST . ';dbname=' . DB_NAME . ';charset=' . DB_CHARSET, DB_USER, DB_PASSWORD);
   }
