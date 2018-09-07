a58bac9b51affb4108c879d124040fdc4d31d7e9
jeromesnail <jeromesnail@hotmail.fr>
2018-09-07 17:37:57 +0200

    Added AdminManager class

---
+++ models/AdminManager.php
@@ -0,0 +1,27 @@
+<?php
+
+class AdminManager extends Manager {
+
+  public function getAdmin() {
+    $db = $this->dbConnect();
+    $req = $db->query('SELECT * FROM admin ORDER BY createdAt DESC');
+    $admin = new Admin($req->fetch(PDO::FETCH_ASSOC));
+    $req->closeCursor();
+
+    return $admin;
+  }
+
+  public function editAdmin(Admin $admin) {
+    $db = $this->dbConnect();
+    $req = $db->prepare('INSERT INTO admin(createdAt, login, email, password, displayName) VALUES(NOW(), :login, :email, :password, :displayName)');
+    $affectedLines = $req->execute([
+      ':login' => $admin->login(),
+      ':email' => $admin->email(),
+      ':password' => $admin->password(),
+      ':displayName' => $admin->displayName()
+    ]);
+    $req->closeCursor();
+
+    return $affectedLines;
+  }
+}
