63b572e47e12590b5d62f19f5ba96c864fbcb10b
jeromesnail <jeromesnail@hotmail.fr>
2018-09-12 17:09:43 +0200

    Added displayPostList controller to Front class Added postCount and postRank to manage paging

--- controller/Front.php
+++ controller/Front.php
@@ -8,13 +8,30 @@ class Front {
     $postManager = new \Model\PostManager();
     $commentManager = new \Model\CommentManager();
 
+    $postCount = $postManager->getPostCount();
     $post = $postManager->getPostByRank($rank, $order);
     $comments = $commentManager->getComments($post->id());
 
     $view = new \View\View('view/front/postView.php', [
       'title' => $post->title(),
+      'postRank' => $rank,
+      'postCount' => $postCount,
       'post' => $post,
       'comments' => $comments
+    ]);
+
+    $view->output();
+  }
+
+
+  public function displayPostList() {
+    $postManager = new \Model\PostManager();
+
+    $posts = $postManager->getPosts();
+
+    $view = new \View\View('view/front/postListView.php', [
+      'title' => 'Liste des chapitres',
+      'posts' => $posts
     ]);
 
     $view->output();
