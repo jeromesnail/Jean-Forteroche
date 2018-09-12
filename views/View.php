171bf462125595481f8cce0d2a75858de1d78e68
jeromesnail <jeromesnail@hotmail.fr>
2018-09-12 13:39:49 +0200

    Created View class

---
+++ views/View.php
@@ -0,0 +1,38 @@
+<?php
+
+class View {
+
+  // Private attributes
+  private $_file,
+          $_data;
+
+  
+  // Setters
+  private function setFile($file) {
+    if (file_exists($file)) {
+      $this->_file = $file;
+    }
+  }
+
+  private function setData($data) {
+    if (is_array($data)) {
+      $this->_data = $data;
+    }
+  }
+
+  // Constructor
+  public function __construct($file, $data) {
+    $this->setFile($file);
+    $this->setData($data);
+  }
+
+
+  public function output() {
+    extract($this->_data);
+
+    ob_start();
+    require($this->_file);
+    $content = ob_get_clean();
+    require('template.php');
+  }
+}
