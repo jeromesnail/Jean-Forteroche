849b1471cf1ca6dea7e6b04eaac5536af54d914c
jeromesnail <jeromesnail@hotmail.fr>
2018-09-12 12:12:43 +0200

    Added title attribute to Post class

--- models/Post.php
+++ models/Post.php
@@ -3,10 +3,15 @@
 class Post extends Entry {
 
   // Private attributes
-  private $_content,
+  private $title,
+          $_content,
           $_editedAt;
 
   // Getters
+  public function title() {
+    return $this->_title;
+  }
+
   public function content() {
     return $this->_content;
   }
@@ -16,6 +21,13 @@ class Post extends Entry {
   }
 
   // Setters
+  public function setTitle(string $title) {
+    if (empty($title) || strlen($title > 255)) {
+      throw new Exception('Invalid title');
+    }
+    $this->_title = $title;
+  }
+
   public function setContent(string $content) {
     if (empty($content)) {
       throw new Exception('Content cannot be empty');
