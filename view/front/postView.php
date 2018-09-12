afd9bbe5da982f8946091ae615b48ad3a08465ea
jeromesnail <jeromesnail@hotmail.fr>
2018-09-12 18:38:40 +0200

    Edited paging in postView to use boostrap

--- view/front/postView.php
+++ view/front/postView.php
@@ -1,26 +1,28 @@
 <?php
 $previousAttribute = $postRank > 1 ?
-  'href="index.php?action=displayPost&rank=' . ($postRank - 1) . '"' :
-  'class="disabled"';
+  'href="index.php?action=displayPost&rank=' . ($postRank - 1) . '" class="col-4 btn btn-outline-primary"' :
+  'href="#" class="col-4 btn btn-outline-primary disabled"';
 
 $nextAttribute = $postRank < $postCount ?
-  'href="index.php?action=displayPost&rank=' . ($postRank + 1) . '"' :
-  'class="disabled"';
-  ?>
+  'href="index.php?action=displayPost&rank=' . ($postRank + 1) . '" class="col-4 btn btn-outline-primary"' :
+  'href="#" class="col-4 btn btn-outline-primary disabled"';
+?>
 
 <section id="post">
-  <div id="paging">
-    <a id="previous" <?= $previousAttribute ?>>
+  <div class="row text-center">
+    <a <?= $previousAttribute ?>>
       <i class="fas fa-angle-double-left"></i>
     </a>
-    <div>Page <?= $postRank ?>/<?= $postCount ?></div>
-    <a id="next" <?= $nextAttribute ?>>
+    <div class="col-4">Page <?= $postRank ?>/<?= $postCount ?></div>
+    <a <?= $nextAttribute ?>>
       <i class="fas fa-angle-double-right"></i>
     </a>
   </div>
   
-  <h1><?= $post->title() ?></h1>
-  <p><?= $post->content() ?></p>
+  <div class="jumbotron">
+    <h1><?= htmlspecialchars($post->title()) ?></h1>
+    <p><?= htmlspecialchars($post->content()) ?></p>
+  </div>
 </section>
 
 <section id="comments">
@@ -35,4 +37,25 @@ $nextAttribute = $postRank < $postCount ?
   <?
   }
   ?>
+</section>
+
+<section id="comment-form">
+  <h2>Réagissez à ce chapitre :</h2>
+  <form action="index.php?action=addComment" method="post">
+    <input type="hidden" name="postId" value="<?= $post->id() ?>" >
+    <input type="hidden" name="postRank" value="<?= $postRank ?>" >
+    <div class="form-group">
+      <label for="name">Votre nom :</label>
+      <input class="form-control" id="name" name="name" placeholder="Lettre et chiffres uniquement" type="text" />
+    </div>
+    <div class="form-group">
+      <label for="email">Votre adresse email :</label>
+      <input class="form-control" id="email" name="email" placeholder="adresse@example.com" type="email" />
+    </div>
+    <div class="form-group">
+      <label for="message">Votre message :</label>
+      <textarea class="form-control" name="message" id="message" cols="30" rows="10"></textarea>
+    </div>
+    <button type="submit" class="btn btn-primary">Envoyer</button> 
+  </form>
 </section>
