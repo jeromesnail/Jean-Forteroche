cd2f3242a64dd50d4539186e58ba891ad0951c03
jeromesnail <jeromesnail@hotmail.fr>
2018-09-13 16:09:59 +0200

    Put the comment related methods in the right controller

--- controller/PostController.php
+++ controller/PostController.php
@@ -38,33 +38,4 @@ class PostController {
 
     $view->output();
   }
-
-  public function submitComment($postId, $postRank, $order, $name, $email, $message) {
-    $commentManager = new \Model\CommentManager();
-
-    $comment = new \Model\Comment([
-      'postId' => $postId,
-      'name' => $name,
-      'email' => $email,
-      'message' => $message
-    ]);
-
-    $affectedLines = $commentManager->addComment($comment);
-    if ($affectedLines) {
-      header('Location: index.php?action=displayPost&rank=' . $postRank . '&order=' . $order);
-    } else {
-      throw new \Exception('Database error, could not add the comment');
-    }
-  }
-
-  public function submitReport($commentId, $postRank, $order) {
-    $commentManager = new \Model\CommentManager();
-
-    $affectedLines = $commentManager->addReport($commentId);
-    if ($affectedLines) {
-      header('Location: index.php?action=displayPost&rank=' . $postRank . '&order=' . $order);
-    } else {
-      throw new \Exception('Database error, could not report the comment');
-    }
-  }
 }
