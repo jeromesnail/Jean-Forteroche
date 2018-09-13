a0f6808ad902902886d5a46d30b35d0b4e86f551
jeromesnail <jeromesnail@hotmail.fr>
2018-09-13 16:47:50 +0200

    Fixed Exception namespace

--- model/Admin.php
+++ model/Admin.php
@@ -32,28 +32,28 @@ class Admin extends Entry {
     // login can only contain between 4 and 16 word characters 
     $regexp = '/^[\w]{4,16}$/';
     if (!preg_match($regexp, $login)) {
-      throw new Exception('Invalid login format');
+      throw new \Exception('Invalid login format');
     }
     $this->_login = $login;
   }
 
   public function setEmail(string $email) {
     if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-      throw new Exception('Invalid email');
+      throw new \Exception('Invalid email');
     }
     $this->_email = $email;
   }
 
   public function setPassword(string $password) {
     if (strlen($password) <> 60) {
-      throw new Exception('Invalid password'); 
+      throw new \Exception('Invalid password'); 
     }
     $this->_password = $password;
   }
 
   public function setDisplayName(string $displayName) {
     if (strlen($displayName) > 32) {
-      throw new Exception('Invalid display name');      
+      throw new \Exception('Invalid display name');      
     }
     $this->_displayName = $displayName;
   }
