bc2aeb26dc7544237d262bdc577abe6a3599a030
jeromesnail <jeromesnail@hotmail.fr>
2018-09-14 16:56:01 +0200

    Added loginView (draft)

---
+++ view/back/loginView.php
@@ -0,0 +1,11 @@
+<form action="index.php?action=loginPost", method="post">
+  <label for="login">Login :</label>
+  <input id="login" name="login" type="text" />
+  <label for="password">Mot de passe :</label>
+  <input id="password" name="password" type="password" />
+  <input id="remember" name="remember" type="checkbox">
+  <label for="remember">Se souvenir de moi</label>
+  <button type="submit">Valider</button>
+</form>
+
+<a href="index.php">Revenir au blog</a>
